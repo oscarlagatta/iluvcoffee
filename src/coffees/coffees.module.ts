46aa9b96767bfcd77352ab03de775a6af441825e
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-07-08 14:38:12 +0100

    class providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,11 +7,22 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+// class ConfigService {}
+// class DevelopmentConfigService {}
+// class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    // {
+    //   provide: ConfigService,
+    //   useClass:
+    //     process.env.NODE_ENV === 'development'
+    //       ? DevelopmentConfigService
+    //       : ProductionConfigService,
+    // },
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
   ],
   exports: [CoffeesService],
