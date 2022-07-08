56cd2b2a68e2ea1601021ac62dd578c44f990775
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-07-08 14:44:03 +0100

    factory providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
@@ -10,6 +10,14 @@ import { Flavor } from './entities/flavor.entity';
 // class ConfigService {}
 // class DevelopmentConfigService {}
 // class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    /** do something */
+    return ['Starbucks', 'Dunkin Donuts', 'Staropramen'];
+  }
+}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
@@ -23,7 +31,13 @@ import { Flavor } from './entities/flavor.entity';
     //       ? DevelopmentConfigService
     //       : ProductionConfigService,
     // },
-    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+    CoffeeBrandsFactory,
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (coffeeBrandsFactory: CoffeeBrandsFactory) =>
+        coffeeBrandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    },
   ],
   exports: [CoffeesService],
 })
