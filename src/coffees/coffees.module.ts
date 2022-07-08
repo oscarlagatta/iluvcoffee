3b3e4a1b1129b9226599bdbf826638166be91861
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-07-08 15:25:49 +0100

    async providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,7 @@
 import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { Connection, DataSource } from 'typeorm';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
@@ -34,9 +35,16 @@ export class CoffeeBrandsFactory {
     CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (coffeeBrandsFactory: CoffeeBrandsFactory) =>
-        coffeeBrandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      useFactory: async (dataSource: DataSource): Promise<string[]> => {
+        // const coffeeBrands = await dataSource.query('SELECT * FROM coffee_brands');
+        const coffeeBrands = await Promise.resolve([
+          'Starbucks',
+          'Dunkin Donuts',
+          'Staropramen',
+        ]);
+        console.log('[!] Async factory');
+        return coffeeBrands;
+      },
     },
   ],
   exports: [CoffeesService],
