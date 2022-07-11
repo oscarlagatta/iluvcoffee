96b3438e4c4b95afe9aad5adae8aef614d7dadd9
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-07-11 18:21:51 +0100

    adds request logging with middleware

---
+++ src/common/middleware/logging.middleware.ts
@@ -0,0 +1,18 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    // calculate the time it takes to execute the request response
+
+    console.time('Request-response time');
+    console.log('Hi from middleware!');
+
+    // we hook to the express finish event
+    res.on('finish', () => console.timeEnd('Request-response time'));
+
+    // we could long lasting methods to the database and keep track of how long 
+    // every API takes to complete.
+    next();
+  }
+}
