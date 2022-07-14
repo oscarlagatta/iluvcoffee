76e92e3ed66ef3658bd6e50621094a21f74c1942
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-07-14 14:26:46 +0100

    adds mongoose and nestjs/mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import appConfig from './app.config';
 import { AppController } from './app.controller';
@@ -21,18 +22,19 @@ import { CommonModule } from './common/common.module';
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/iluvcoffee'),
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: () => ({
+    //     type: 'postgres',
+    //     host: process.env.DATABASE_HOST,
+    //     port: +process.env.DATABASE_PORT,
+    //     username: process.env.DATABASE_USER,
+    //     password: process.env.DATABASE_PASSWORD,
+    //     database: process.env.DATABASE_NAME,
+    //     autoLoadEntities: true,
+    //     synchronize: true,
+    //   }),
+    // }),
     CoffeeRatingModule,
     CommonModule,
   ],
